a9d8f972bf53ea1c8722e4093016d256615d2524
JoaoVitorPerin <joao.vperin@outlook.com>
2026-05-21 14:00:06 -0300

    upload de foto de perfil e ajustes de ux

--- src/app/modules/chat/chat.routes.ts
+++ src/app/modules/chat/chat.routes.ts
@@ -2,11 +2,6 @@ import { Routes } from '@angular/router';
 
 export const CHAT_ROUTES: Routes = [
   {
-    path: '',
-    loadComponent: () =>
-      import('./chat.component').then(c => c.ChatComponent),
-  },
-  {
     path: ':hashPolitian',
     loadComponent: () =>
       import('./chat.component').then(c => c.ChatComponent),
